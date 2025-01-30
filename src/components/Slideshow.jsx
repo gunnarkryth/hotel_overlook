f7b32d86ccd0e02c8db8e66bb7868e3fe4bc505c
Gunski <gunnarkryth@gmail.com>
2025-01-30 09:20:02 +0100

    1

--- src/components/Slideshow.jsx
+++ src/components/Slideshow.jsx
@@ -1,34 +1,64 @@
-import { useFetch } from "../hooks/useFetch";
-import {
-  CircularProgress,
-  Alert,
-  Paper,
-  Typography,
-  List,
-  ListItem,
-  ListItemText,
-} from "@mui/material";
+import { useState, useEffect } from "react";
+import { Box, Paper, IconButton } from "@mui/material";
+import { ArrowBackIos, ArrowForwardIos } from "@mui/icons-material";
+
+// Importér alle billeder i assets/images automatisk
+const images = import.meta.glob("../assets/images/*", { eager: true });
+const imageArray = Object.values(images).map((img) => img.default);
 
 export const Slideshow = () => {
-  const { data, loading, error } = useFetch("http://localhost:4000/imagelist");
+  const [index, setIndex] = useState(0);
 
-  if (loading) return <CircularProgress />;
-  if (error) return <Alert severity="error">{error}</Alert>;
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setIndex((prevIndex) => (prevIndex + 1) % imageArray.length);
+    }, 3000);
+    return () => clearInterval(interval);
+  }, []);
 
-  console.log(data);
+  const nextImage = () => setIndex((prev) => (prev + 1) % imageArray.length);
+  const prevImage = () =>
+    setIndex((prev) => (prev - 1 + imageArray.length) % imageArray.length);
 
   return (
-    <Paper elevation={3} style={{ padding: "20px", margin: "20px" }}>
-      <Typography variant="h4" gutterBottom>
-        Hoteller
-      </Typography>
-      <List>
-        {data?.map((hotel) => (
-          <ListItem key={hotel.id}>
-            <img src="" alt="" srcset="" />
-          </ListItem>
-        ))}
-      </List>
+    <Paper
+      elevation={3}
+      sx={{
+        position: "relative",
+        width: "100%",
+        maxWidth: 600,
+        mx: "auto",
+        overflow: "hidden",
+      }}
+    >
+      <Box
+        component="img"
+        src={imageArray[index]}
+        alt="Slideshow"
+        sx={{ width: "100%", height: "auto" }}
+      />
+      <IconButton
+        onClick={prevImage}
+        sx={{
+          position: "absolute",
+          top: "50%",
+          left: 10,
+          transform: "translateY(-50%)",
+        }}
+      >
+        <ArrowBackIos />
+      </IconButton>
+      <IconButton
+        onClick={nextImage}
+        sx={{
+          position: "absolute",
+          top: "50%",
+          right: 10,
+          transform: "translateY(-50%)",
+        }}
+      >
+        <ArrowForwardIos />
+      </IconButton>
     </Paper>
   );
 };
